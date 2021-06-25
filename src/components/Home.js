3f14f227a2dfe43feec4701738410004fc5f57c0
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-06-25 13:02:59 -0400

    Added Home.js Return

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h2>Home Page</h2>
+      <p>
+        This webapp is to help students see the UW Courses which they are
+        intersted in.
+      </p>
+      <p>To view course information, click on the 'Courses' tab</p>
+      <p>
+        To view a list waterloo campus buildigns, click on the 'Buildings' tab
+      </p>
+    </>
+  );
+};
+
+export default Home;
