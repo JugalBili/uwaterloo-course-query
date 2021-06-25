84314c49378cb890bfdcc86fbd6dab94d289858b
Edmund Lin <edmundjunzhelin@gmail.com>
2021-06-25 14:12:27 -0400

    Added dropdown to locations list

--- src/components/LocationListItem.js
+++ src/components/LocationListItem.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 
 const LocationListItem = ({location}) => {
+    const [showName, setShowName] = useState(false);
 
     const handleLoadLocations = () => {
-
+        setShowName(!showName);
     }
     return (
         <div>
             <a href='#' onClick={handleLoadLocations}>
-                {location.buildingCode}
+                {location.buildingName}
             </a>
+            {showName ?
+            <h3>{location.buildingCode}</h3> :
+            null
+        }
 
         </div>
     );
