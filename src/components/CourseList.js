6d95a1e3cd57f23de6c2a051a720a50edbc212b6
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-06-25 15:53:03 -0400

    Updated CourseList

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -4,7 +4,6 @@ import AddCourse from "./AddCourse.js";
 
 require("dotenv").config({ path: "../../.env" });
 var KEY = process.env.REACT_APP_API_KEY;
-//console.log(KEY);
 
 export default class CourseList extends React.Component {
   constructor(props) {
