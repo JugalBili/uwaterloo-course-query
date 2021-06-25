b33bb61a791ce5a0dd4a5d20468fee716a863bef
Conor Lamont <conor_lamont@outlook.com>
2021-06-25 16:09:50 -0400

    added .env

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,20 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import LocationListItem from './LocationListItem'
+require("dotenv").config({ path: "../../.env" });
+var KEY = process.env.REACT_APP_API_KEY;
+
 const LocationList = () => {
-    const apiKey = 'B705DBD8436C40549510F8704F02DDD6';
+
     const [locations, setLocations] = useState(null);
 
-    const fetchLocations  = () => {
+    const fetchLocations = () => {
         fetch(
             'https://openapi.data.uwaterloo.ca/v3/Locations',
             {
                 headers: {
-                    'x-api-key': apiKey,
+                    'x-api-key': KEY,
                 }
             }
         )
-        .then((response) => response.json())
-        .then((result=>setLocations(result)));
+            .then((response) => response.json())
+            .then((result => setLocations(result)));
 
 
     }
@@ -24,12 +27,12 @@ const LocationList = () => {
     }, []);
     return (
         <>
-           <h2>List of UW Buildings</h2> 
-           {locations && locations.map((location) => (
-               <LocationListItem key={location.buildingID} location={location} />
-           ))
+            <h2>List of UW Buildings</h2>
+            {locations && locations.map((location) => (
+                <LocationListItem key={location.buildingID} location={location} />
+            ))
 
-           }
+            }
         </>
     )
 }
