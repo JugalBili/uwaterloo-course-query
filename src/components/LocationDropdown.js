464d4eab6cee50154643ef6b7567f22dbd83942e
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-06-27 15:20:37 -0400

    Created Locations-jugal

--- src/components/LocationDropdown.js
+++ src/components/LocationDropdown.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
 
+const LocationDropdown = ({ onLocationChange }) => {
+  const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = () => setIsOpen(!isOpen);
 
-const LocationDropdown = () => {
-    const dropdownRef = useRef(null);
-    const[isOpen, setIsOpen] = useState(false);
-    const openMenu = () => setIsOpen(!isOpen);
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const handleLocationChange = (index) => {
+    onLocationChange(index);
+  };
 
-export default LocationDropdown
+  return (
+    <div className="location-select">
+      <select
+        name="location-type"
+        id="location-type"
+        onChange={handleLocationChange(this.selectedIndex())}
+      >
+        <option value="null" selected>
+          Academic
+        </option>
+        <option value="CGR">CGR</option>
+        <option value="CLN">CLN</option>
+        <option value="CLV">CLV</option>
+        <option value="MKV">MKV</option>
+        <option value="REN">REN</option>
+        <option value="REV">REV</option>
+        <option value="STJ">STJ</option>
+        <option value="STP">STP</option>
+        <option value="UWP">UWP</option>
+        <option value="V1">V1</option>
+      </select>
+    </div>
+  );
+};
+
+export default LocationDropdown;
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import LocationListItem from "./LocationListItem";
+import LocationDropdown from "./LocationDropdown";
 
 require("dotenv").config({ path: "../../.env" });
 var KEY = process.env.REACT_APP_API_KEY;
-var groupBy = require('lodash.groupby');
+var groupBy = require("lodash.groupby");
 const LocationList = () => {
   const [locations, setLocations] = useState(null);
+  const [selectedLocations, setSelectedLocations] = useState(null);
+  setSelectedLocations(locations);
+  var grouped = null;
+  console.log("called again");
 
   const fetchLocations = () => {
     fetch("https://openapi.data.uwaterloo.ca/v3/Locations", {
@@ -14,21 +19,61 @@ const LocationList = () => {
       },
     })
       .then((response) => response.json())
-      .then((result) => setLocations(result));
+      .then((result) => {
+        setLocations(result);
+        groupBy(locations, function (loc) {
+          return loc.parentBuildingCode;
+        });
+      });
   };
 
   useEffect(() => {
     fetchLocations();
   }, []);
 
-  var grouped = groupBy(locations, function(loc) {
+  /*
+  var grouped = groupBy(locations, function (loc) {
     return loc.parentBuildingCode;
   });
-  var nullCode = grouped[null]
-console.log(nullCode)
+  var nullCode = grouped[null];
+  console.log(nullCode);
+  console.log(grouped);
+  */
+  /*
+  const handleLocationChange = (index) => {
+    if (index === "CGR") {
+      setSelectedLocations(grouped.CGR);
+    } else if (index === "CLN") {
+      setSelectedLocations(grouped.CLN);
+    } else if (index === "CLV") {
+      setSelectedLocations(grouped.CLV);
+    } else if (index === "MKV") {
+      setSelectedLocations(grouped.MKV);
+    } else if (index === "REN") {
+      setSelectedLocations(grouped.REN);
+    } else if (index === "REV") {
+      setSelectedLocations(grouped.REV);
+    } else if (index === "STJ") {
+      setSelectedLocations(grouped.STJ);
+    } else if (index === "STP") {
+      setSelectedLocations(grouped.STP);
+    } else if (index === "UWP") {
+      setSelectedLocations(grouped.UWP);
+    } else if (index === "V1") {
+      setSelectedLocations(grouped.V1);
+    } else if (index === "null") {
+      setSelectedLocations(grouped.null);
+    }
+  };
+*/
   return (
     <div className="locations">
       <h2 className="Location-Heading">List of UW Buildings</h2>
+      <LocationDropdown
+        onLocationChange={(index) => {
+          this.handleLocationChange(index);
+        }}
+      />
       {locations &&
         locations.map((location) => (
           <LocationListItem key={location.buildingID} location={location} />
