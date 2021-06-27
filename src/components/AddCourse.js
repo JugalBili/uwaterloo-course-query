5f7a26897607d40148291369b3459b9e90f32863
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-06-27 17:33:52 -0400

    Add Course on Enter

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -19,7 +19,7 @@ const AddCourse = (props) => {
     setCourseName(""); // Clears text field
   };
 
-  const handleKeyDown = (e) => {
+  const _handleKeyDown = (e) => {
     if (e.key === "Enter") {
       handleAddCourse();
     }
@@ -32,7 +32,7 @@ const AddCourse = (props) => {
         type="text"
         value={courseName}
         onChange={handleChangeName}
-        onKeyPress={handleKeyDown}
+        onKeyPress={_handleKeyDown}
       />
       <button id="button" onClick={handleAddCourse}>
         Add Course
