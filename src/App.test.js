0624877ca871639af2afec459851cfb1dfbaecd6
Matthew Murison <matthew@matnico.com>
2021-06-29 14:58:03 -0400

    idk

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "./components/Home";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("render h2 elements", () => {
+  render(<Home />);
+
+  screen.debug();
+
+  expect(screen.getByText("Home Page")).toBeInTheDocument();
+});
+
+test("render p elements", () => {
+  render(<Home />);
+
+  screen.debug();
+
+  expect(
+    screen.getByText(
+      "This webapp is to help students see the UW Courses and Locations which they are interested in."
+    )
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText("To view course information, click on the 'Courses' tab")
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      "To view a list of waterloo campus locations, click on the 'Locations' tab"
+    )
+  ).toBeInTheDocument();
 });
