4e63fef40929f647b98f3d5b675e74fc48a63547
Matthew Murison <matthew@matnico.com>
2021-06-29 19:33:38 -0400

    Fixed Tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "../components/Home";
+import AddCourse from "../components/AddCourse"
+import LocationDropdown from "../components/LocationDropdown"
 
 test("render h2 elements", () => {
   render(<Home />);
-
-  screen.debug();
 
   expect(screen.getByText("Home Page")).toBeInTheDocument();
 });
 
 test("render p elements", () => {
   render(<Home />);
-
-  screen.debug();
 
   expect(
     screen.getByText(
@@ -28,4 +26,16 @@ test("render p elements", () => {
       "To view a list of waterloo campus locations, click on the 'Locations' tab"
     )
   ).toBeInTheDocument();
+});
+
+test("render building group p element", () => {
+  render(<LocationDropdown />);
+
+  expect(screen.getByText("Select a building group from the dropdown")).toBeInTheDocument();
+});
+
+test("check for input ID in AddCourse", () => {
+  render(<AddCourse />);
+
+  expect(document.getElementById("input")).toBeInTheDocument();
 });
