8b31bf9e45533358b655adedad247a6dee267f41
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-06-29 12:15:32 -0400

    Updated Home Text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,15 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
-      <h2 >Home Page</h2>
+      <h2>Home Page</h2>
       <p>
-        This webapp is to help students see the UW Courses which they are
-        intersted in.
+        This webapp is to help students see the UW Courses and Locations which
+        they are interested in.
       </p>
       <p>To view course information, click on the 'Courses' tab</p>
       <p>
-        To view a list waterloo campus buildigns, click on the 'Buildings' tab
+        To view a list of waterloo campus locations, click on the 'Locations'
+        tab
       </p>
     </div>
   );
