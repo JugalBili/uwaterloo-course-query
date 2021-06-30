1899a29b790b74ad575777604db86135c292dcbf
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-06-30 16:10:20 -0400

    Updated Tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "../components/Home";
+import AddCourse from "../components/AddCourse";
+import LocationDropdown from "../components/LocationDropdown";
+import { shallow } from "../../enzyme";
 
 test("render h2 elements", () => {
   render(<Home />);
-
-  screen.debug();
 
   expect(screen.getByText("Home Page")).toBeInTheDocument();
 });
 
 test("render p elements", () => {
   render(<Home />);
-
-  screen.debug();
 
   expect(
     screen.getByText(
@@ -28,4 +27,33 @@ test("render p elements", () => {
       "To view a list of waterloo campus locations, click on the 'Locations' tab"
     )
   ).toBeInTheDocument();
+});
+
+test("render building group p element", () => {
+  render(<LocationDropdown />);
+
+  expect(
+    screen.getByText("Select a building group from the dropdown")
+  ).toBeInTheDocument();
+});
+
+it("should test add course list page has appropriate text", () => {
+  const wrapper = shallow(<AddCourse />);
+  wrapper.find("button").simulate("click");
+  expect(wrapper.find("input").text()).toEqual("");
+  expect(wrapper.find("button").text()).toEqual("Add Course");
+  // expect(wrapper.find(LocationList)).toHaveLength(1);
+  //   expect(wrapper.find(AddCourse)).toHaveLength(0);
+});
+it("should test that the dropdown has 12 options", () => {
+  const wrapper = shallow(<LocationDropdown />);
+  //wrapper.find('location-type').simulate('click');
+  console.log(wrapper.debug());
+  expect(wrapper.find("option")).toHaveLength(12);
+});
+
+test("check for input ID in AddCourse", () => {
+  render(<AddCourse />);
+
+  expect(document.getElementById("input")).toBeInTheDocument();
 });
